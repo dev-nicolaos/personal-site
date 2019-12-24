86386262f131b75eb23747f0a4c0ac5a77a00404
dev-nicolaos <contact@nicolaos.dev>
2019-12-24 12:58:29 -0800

    js formatting cleanup

--- dist/js/utility-belt.js
+++ dist/js/utility-belt.js
@@ -1,6 +1,7 @@
 import { utilityBeltItems } from "./items.js";
 
-const mapObject = (obj, func) => Object.keys(obj).map((key, index) => func(obj[key], key, index, obj));
+const mapObject = (obj, func) =>
+  Object.keys(obj).map((key, index) => func(obj[key], key, index, obj));
 
 const renderAccordionSection = (items, sectionName, index) => `
   <details class="accordion-section" ${index === 0 ? "open" : ""}>
@@ -11,11 +12,13 @@ const renderAccordionSection = (items, sectionName, index) => `
       </h2>
     </summary>
     <ul class="accordion-section-list">
-      ${items.reduce((html, item) => html += `
-        <li class="accordion-list-item">
-          <a target="_blank" href="${item.url}" rel="noopener">${item.name}</a>
-        </li>
-      `, '')}
+      ${items.reduce(
+        (html, item) => (html += `
+          <li class="accordion-list-item">
+            <a target="_blank" href="${item.url}" rel="noopener">${item.name}</a>
+          </li>
+        `), "",
+      )}
     </ul>
   </details>
 `;
@@ -24,23 +27,25 @@ class UtilityBelt extends HTMLElement {
   constructor() {
     super();
     this.innerHTML = mapObject(utilityBeltItems, renderAccordionSection).join("");
-    this.querySelectorAll("summary").forEach(el => el.addEventListener("click", e => {
-      e.preventDefault();
-      const mobile = getComputedStyle(this, "::after").content === "none";
-      const currentlyOpen = this.querySelector("[open]");
-      const targetAlreadyOpen = e.currentTarget.parentElement === currentlyOpen;
+    this.querySelectorAll("summary").forEach(el =>
+      el.addEventListener("click", e => {
+        e.preventDefault();
+        const mobile = getComputedStyle(this, "::after").content === "none";
+        const currentlyOpen = this.querySelector("[open]");
+        const targetAlreadyOpen = e.currentTarget.parentElement === currentlyOpen;
 
-      if (mobile) {
-        e.currentTarget.parentElement.toggleAttribute("open");
-        if (!targetAlreadyOpen) {
+        if (mobile) {
+          e.currentTarget.parentElement.toggleAttribute("open");
+          if (!targetAlreadyOpen) {
+            currentlyOpen.removeAttribute("open");
+          }
+        } else if (!targetAlreadyOpen) {
+          e.currentTarget.parentElement.toggleAttribute("open");
           currentlyOpen.removeAttribute("open");
         }
-      } else if (!targetAlreadyOpen) {
-        e.currentTarget.parentElement.toggleAttribute("open");
-        currentlyOpen.removeAttribute("open");
-      }
-    }));
+      }),
+    );
   }
 }
 
-customElements.define('utility-belt', UtilityBelt);
+customElements.define("utility-belt", UtilityBelt);
