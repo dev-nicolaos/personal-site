0bb25d1ca8c5f4d8221fcdb4f37c1fbe614fce06
dev-nicolaos <contact@nicolaos.dev>
2022-01-06 07:36:59 -0800

    convert site header to block

---
+++ src/blocks/site-header.js
@@ -0,0 +1,32 @@
+export default /*html*/ `
+  <header class="site-header">
+    <img alt="" class="site-header_logo" height="1" src="/assets/img/logo.svg" width="1" />
+    <nav>
+      <ul class="site-header_nav">
+        <li>
+          <a
+            class="link"
+            href="https://www.linkedin.com/in/nicolaos-skimas"
+            target="_blank"
+            rel="noopener"
+            >
+            LinkedIn
+          </a>
+        </li>
+
+        <li>
+          <a
+            class="link"
+            href="https://github.com/dev-nicolaos"
+            target="_blank"
+            rel="noopener"
+            >
+            GitHub
+          </a>
+        </li>
+
+        <li><a class="link" href="/resume">Resume</a></li>
+      </ul>
+    </nav>
+  </header>
+`;
