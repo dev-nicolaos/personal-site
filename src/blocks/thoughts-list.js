5766786b30e5f0da5b4a39ff9bac714134a261da
dev-nicolaos <contact@nicolaos.dev>
2022-01-26 02:48:05 -0800

    Add thoughts (#19)

---
+++ src/blocks/thoughts-list.js
@@ -0,0 +1,21 @@
+import thoughtsMetaData from "./thoughts-meta-data.json" assert { type: "json" };
+
+const createListing = ([url, listing]) => /*html*/`
+  <li class="thought-listing">
+    <a class="link thought-listing_title" href="/thoughts/${url}">
+      ${listing.title}
+    </a>
+    <time class="thought-listing_date datetime="${listing.publishDate}">${listing.publishDate}</time>
+  </li>
+`;
+
+const sortListings = ([url1, entry1], [url2, entry2]) =>
+  Date.parse(entry1.publishDate) - Date.parse(entry2.publishDate);
+
+const listings = Object
+  .entries(thoughtsMetaData)
+  .sort(sortListings)
+  .map(createListing)
+  .join("");
+
+export default /* html */`<ul class="thoughts-list">${listings}</ul>`;
