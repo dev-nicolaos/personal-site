9a43a4488a06ae18a19f78fbbe9467e36ee69602
dev-nicolaos <contact@nicolaos.dev>
2022-01-27 16:43:24 -0800

    pass page slug to function blocks

--- build-system.ts
+++ build-system.ts
@@ -10,7 +10,7 @@ const BUILD_FOLDER_NAME = "dist";
 const BLOCKS_FOLDER_NAME = "blocks";
 const BLOCKS_FOLDER_PATH = `./${SOURCE_FOLDER_NAME}/${BLOCKS_FOLDER_NAME}`;
 
-type BlockFunc = () => string;
+type BlockFunc = (pageSlug: string) => string;
 type BlocksDict = { [blockName: string]: BlockFunc | string };
 
 async function getBlockFuncs(): Promise<BlocksDict> {
@@ -35,15 +35,30 @@ async function getBlockFuncs(): Promise<BlocksDict> {
 const getBuildPath = (sourcePath: string) =>
   sourcePath.replace(SOURCE_FOLDER_NAME, BUILD_FOLDER_NAME);
 
-const hydrateBlock = (blocks: BlocksDict) => (match: string) => {
+const hydrateBlock = (blocks: BlocksDict, pageSlug: string) => (match: string) => {
   const invokedName = match.slice(2, -2).trim();
   const maybeBlock = blocks[invokedName];
   if (!maybeBlock) return match;
-  return (typeof maybeBlock === "string" ? maybeBlock : maybeBlock()).trim();
+  return (typeof maybeBlock === "string" ? maybeBlock : maybeBlock(pageSlug)).trim();
 };
 
-const hydrateBlocksInFile = (file: string, blocks: BlocksDict) =>
-  file.replaceAll(/{{ \S+ }}/g, hydrateBlock(blocks));
+const hydrateBlocksInFile = (file: string, blocks: BlocksDict, pageSlug: string) =>
+  file.replaceAll(/{{ \S+ }}/g, hydrateBlock(blocks, pageSlug));
+
+const getSlug = (path: string) => {
+  const pathSep = Deno.build.os === 'windows' ? '\\' : '/';
+
+  if (!path.endsWith(`${pathSep}index.html`)) {
+    throw Error(`All pages must live at /slug/index.html, encountered ${path}`);
+  }
+
+  const slugEndIndex = path.length - 11; // /index.html
+  const slugStartIndex = path.lastIndexOf(pathSep, slugEndIndex - 1) + 1;
+
+  const folderName = path.slice(slugStartIndex, slugEndIndex);
+
+  return folderName === 'src' ? '/' : folderName;
+}
 
 export async function buildHTMLFiles(htmlPaths: string[]) {
   const blocks = await getBlockFuncs();
@@ -51,6 +66,7 @@ export async function buildHTMLFiles(htmlPaths: string[]) {
     const outFile = hydrateBlocksInFile(
       Deno.readTextFileSync(path),
       blocks,
+      getSlug(path),
     );
 
     const buildPath = getBuildPath(path);
