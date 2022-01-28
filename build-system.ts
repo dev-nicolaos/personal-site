5770e4ca52b8aaf732cdd53105f471dab39e4f27
dev-nicolaos <contact@nicolaos.dev>
2022-01-27 22:49:23 -0800

    reorganize folder structure

--- build-system.ts
+++ build-system.ts
@@ -4,10 +4,13 @@ import {
   ensureFileSync,
   walkSync,
 } from "https://deno.land/std@0.122.0/fs/mod.ts";
+import { SEP } from "https://deno.land/std@0.122.0/path/separator.ts";
 
 const SOURCE_FOLDER_NAME = "src";
 const BUILD_FOLDER_NAME = "dist";
 const BLOCKS_FOLDER_NAME = "blocks";
+const PAGES_FOLDER_NAME = "pages";
+const STATIC_FOLDER_NAME = "www";
 const BLOCKS_FOLDER_PATH = `./${SOURCE_FOLDER_NAME}/${BLOCKS_FOLDER_NAME}`;
 
 type BlockFunc = (pageSlug: string) => string;
@@ -32,8 +35,9 @@ async function getBlockFuncs(): Promise<BlocksDict> {
   return blocks;
 }
 
+const sourcePathRegex = `^${SOURCE_FOLDER_NAME}\\${SEP}(${PAGES_FOLDER_NAME}|${STATIC_FOLDER_NAME})`;
 const getBuildPath = (sourcePath: string) =>
-  sourcePath.replace(SOURCE_FOLDER_NAME, BUILD_FOLDER_NAME);
+  sourcePath.replace(new RegExp(sourcePathRegex), BUILD_FOLDER_NAME);
 
 const hydrateBlock = (blocks: BlocksDict, pageSlug: string) => (match: string) => {
   const invokedName = match.slice(2, -2).trim();
@@ -46,23 +50,21 @@ const hydrateBlocksInFile = (file: string, blocks: BlocksDict, pageSlug: string)
   file.replaceAll(/{{ \S+ }}/g, hydrateBlock(blocks, pageSlug));
 
 const getSlug = (path: string) => {
-  const pathSep = Deno.build.os === 'windows' ? '\\' : '/';
-
-  if (!path.endsWith(`${pathSep}index.html`)) {
+  if (!path.endsWith(`${SEP}index.html`)) {
     throw Error(`All pages must live at /slug/index.html, encountered ${path}`);
   }
 
   const slugEndIndex = path.length - 11; // /index.html
-  const slugStartIndex = path.lastIndexOf(pathSep, slugEndIndex - 1) + 1;
+  const slugStartIndex = path.lastIndexOf(SEP, slugEndIndex - 1) + 1;
 
   const folderName = path.slice(slugStartIndex, slugEndIndex);
 
   return folderName === 'src' ? '/' : folderName;
 }
 
-export async function buildHTMLFiles(htmlPaths: string[]) {
+export async function buildPages(pageSourcePaths: string[]) {
   const blocks = await getBlockFuncs();
-  htmlPaths.forEach((path) => {
+  pageSourcePaths.forEach((path) => {
     const outFile = hydrateBlocksInFile(
       Deno.readTextFileSync(path),
       blocks,
@@ -84,11 +86,11 @@ const getAllSourceFilePaths = (filterFn?: FilterFunction): string[] => {
   return filterFn ? paths.filter(filterFn) : paths;
 };
 
-const getAllHTMLFilePaths = () =>
-  getAllSourceFilePaths((path) => path.endsWith(".html"));
+const getAllPagePaths = () =>
+  getAllSourceFilePaths((path) => path.startsWith(`src${SEP}pages`));
 
-export async function buildAllHTMLFiles() {
-  await buildHTMLFiles(getAllHTMLFilePaths());
+export async function buildAllPages() {
+  await buildPages(getAllPagePaths());
 }
 
 export function copyStaticFile(filePath: string) {
@@ -98,7 +100,7 @@ export function copyStaticFile(filePath: string) {
 }
 
 export const filterStaticPaths = (path: string) =>
-  !path.endsWith(".html") && !path.includes(BLOCKS_FOLDER_NAME);
+  path.startsWith(`src${SEP}www`);
 
 function copyAllStaticFiles() {
   const staticFilePaths = getAllSourceFilePaths(filterStaticPaths);
@@ -107,6 +109,6 @@ function copyAllStaticFiles() {
 
 export async function buildSite() {
   emptyDirSync(BUILD_FOLDER_NAME);
-  await buildAllHTMLFiles();
+  await buildAllPages();
   copyAllStaticFiles();
 }
