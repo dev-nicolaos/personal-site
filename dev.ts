4331d6461a6d203e9a1aaf311896816884f6fd24
dev-nicolaos <contact@nicolaos.dev>
2022-03-30 17:10:18 -0700

    refine dual process management

--- dev.ts
+++ dev.ts
@@ -8,6 +8,8 @@ import {
   filterStaticPaths,
 } from "./build-system.ts";
 
+import { startServer, killServer } from "./devServer.ts";
+
 const handleChange = debounce(async (event: Deno.FsEvent) => {
   console.log(
     new Date().toLocaleTimeString(),
@@ -35,17 +37,40 @@ await buildSite();
 console.log("Site built successfully!");
 
 console.log("Starting dev server...");
-const devCommand = ["netlify", "dev"].concat(Deno.args);
-const netlifyServer = Deno.run({
-  cmd: Deno.build.os === "windows" ? ["cmd", "/c"].concat(devCommand) : devCommand,
-  stderr: "piped",
-});
+const server = startServer();
 
-netlifyServer.stderrOutput().then(err => {
-  const decodedError = new TextDecoder().decode(err);
-  console.error("Dev server errored with error:", decodedError);
+// BEGIN HANDLING SHUTDOWN
+
+/**
+ * We have two processes to manage:
+ *  - This Deno process which watches for changes to the site source code and rebuilds
+ *  - The server process we spawn w/Deno.run which serves completed build folder
+ * We want to make sure that if one process terminates, the other does as well.
+ * Place any code needed to ensure this behavior in this section
+ */
+
+/**
+ * Free Wins
+ *  - Because the server iherits stdin, pressing ctrl+c in the terminal running this process should stop it as well.
+ */
+
+// Kill server if this process ends programattically (e.g. Deno.exit)
+const handleUnload = () => {
+  console.log("Unload event triggered for build watcher, stopping dev server...");
+  killServer(server.pid);
+};
+addEventListener("unload", handleUnload);
+
+// If the server stops (either programmatically or errors out), stop this process
+server.status().then((serverExitStatus) => {
+  if (!serverExitStatus.success) {
+    console.error(`Dev server errored with code ${serverExitStatus.code}, stopping build watcher...`);
+  }
+  removeEventListener("unload", handleUnload);
   Deno.exit();
 });
+
+// END HANDLING SHUTDOWN
 
 const watcher = Deno.watchFs("./src");
 console.log("Watching for changes...");
