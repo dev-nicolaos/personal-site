a706e99dcbd78a1ff110792ca064fab082a8fd0e
dev-nicolaos <contact@nicolaos.dev>
2022-12-01 23:22:22 -0800

    setup rss feed

---
+++ src/pages/thoughts/rss.xml.js
@@ -0,0 +1,21 @@
+import rss from "@astrojs/rss";
+import { filterOutDrafts, sortThoughtsByPublishDate } from "../../utils";
+
+const thoughts = Object.values(import.meta.glob("../**/*.md", { eager: true }))
+  .filter(filterOutDrafts)
+  .sort(sortThoughtsByPublishDate);
+
+export const get = () =>
+  rss({
+    description:
+      "A series of thoughts about life, technology and web development.",
+    items: thoughts.map(({ frontmatter, url }) => ({
+      description: frontmatter.description,
+      link: url,
+      pubDate: frontmatter.publishDate,
+      title: frontmatter.heading,
+    })),
+    site: `${import.meta.env.SITE}thoughts/`,
+    stylesheet: "/assets/pretty-feed-v3.xsl",
+    title: "Nicolaos Skimas",
+  });
