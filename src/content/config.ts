da30ec8c2f24f71724275cec2487362c70f60ce1
dev-nicolaos <contact@nicolaos.dev>
2023-05-02 19:09:42 -0700

    datetime doesn't support date only strings

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,9 @@ const thoughtsCollection = defineCollection({
   schema: z.object({
     description: z.string(),
     draft: z.boolean().optional(),
-    pubDate: z.string().datetime(),
+    // use z.string().date() for pubDate when astro ships a version of zod with support
+    // https://github.com/colinhacks/zod/issues/1676
+    pubDate: z.string(),
     subTitle: z.string().optional(),
     title: z.string(),
   }),
