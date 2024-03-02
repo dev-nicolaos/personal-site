49555f7289f70ff8b5b4d97991e60c5b9ffee792
Nicolaos Skimas <contact@nicolaos.dev>
2024-03-02 09:48:21 -0800

    Collections Migration, Dependency/CI Upgrades (#50)

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,29 @@
 import { z, defineCollection } from "astro:content";
 
+const miscellaneousContentCollection = defineCollection({
+	type: "content",
+});
+
+const siteTechItemsCollection = defineCollection({
+	type: "data",
+	schema: z.object({
+		url: z.string().url(),
+	}),
+});
+
+export const singleLevelResumeSection = z.array(z.string());
+export const dualLevelResumeSection = z.record(
+	z.string(),
+	singleLevelResumeSection,
+);
+
+const resumeSectionsCollection = defineCollection({
+	type: "data",
+	schema: dualLevelResumeSection.or(singleLevelResumeSection),
+});
+
 const thoughtsCollection = defineCollection({
+	type: "content",
 	schema: z.object({
 		description: z.string(),
 		draft: z.boolean().optional(),
@@ -13,5 +36,8 @@ const thoughtsCollection = defineCollection({
 });
 
 export const collections = {
+	miscellaneous: miscellaneousContentCollection,
+	"resume-sections": resumeSectionsCollection,
+	"site-tech": siteTechItemsCollection,
 	thoughts: thoughtsCollection,
 };
