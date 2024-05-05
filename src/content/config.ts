7f4c3ab0f8f08b38bfce89f13716d3a473b5eac7
dev-nicolaos <contact@nicolaos.dev>
2024-05-05 13:48:19 -0700

    bump dependencies

--- src/content/config.ts
+++ src/content/config.ts
@@ -27,9 +27,7 @@ const thoughtsCollection = defineCollection({
 	schema: z.object({
 		description: z.string(),
 		draft: z.boolean().optional(),
-		// use z.string().date() for pubDate when astro ships a version of zod with support
-		// https://github.com/colinhacks/zod/issues/1676
-		pubDate: z.string(),
+		pubDate: z.date(),
 		subTitle: z.string().optional(),
 		title: z.string(),
 	}),
