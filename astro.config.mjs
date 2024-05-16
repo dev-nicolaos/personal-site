19e3da6c4173f25d729ae5e259838e35f86f8b76
dev-nicolaos <contact@nicolaos.dev>
2024-05-16 07:25:33 -0700

    install classnames md plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import rehypeClassNames from "rehype-class-names";
 
 export default defineConfig({
 	integrations: [sitemap()],
 	site: "https://nicolaos.dev",
+	markdown: {
+		rehypePlugins: [
+			[rehypeClassNames, { h2: "secondary-heading", "a[href]": "link" }],
+		],
+	},
 });
