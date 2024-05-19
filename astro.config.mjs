40a9669542ad85bc3c3413b1d94dd82ccd8a6acd
dev-nicolaos <contact@nicolaos.dev>
2024-05-19 10:36:43 -0700

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,33 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import rehypeClassNames from "rehype-class-names";
+import rehypeRaw from "rehype-raw";
 
 export default defineConfig({
 	integrations: [sitemap()],
 	site: "https://nicolaos.dev",
 	markdown: {
+		remarkRehype: {
+			allowDangerousHtml: true,
+		},
 		rehypePlugins: [
-			[rehypeClassNames, { h2: "secondary-heading", "a[href]": "link" }],
+			rehypeRaw,
+			[
+				rehypeClassNames,
+				{
+					"a[href]": "link",
+					blockquote: "blockquote",
+					details: "details",
+					em: "italic",
+					h2: "secondary-heading",
+					h3: "tertiary-heading",
+					hr: "break",
+					":not(pre) code": "inline-code",
+					"ol,ul": "list",
+					p: "paragraph",
+					strong: "bold",
+				},
+			],
 		],
 	},
 });
