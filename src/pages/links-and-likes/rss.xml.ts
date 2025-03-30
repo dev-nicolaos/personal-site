be7718626553064d85fc9ccc47d09f3cb1ac61ea
dev-nicolaos <contact@nicolaos.dev>
2025-03-30 16:14:42 -0700

    fix issue with rss feed

--- src/pages/links-and-likes/rss.xml.ts
+++ src/pages/links-and-likes/rss.xml.ts
@@ -2,18 +2,27 @@ import type { APIRoute } from "astro";
 import rss from "@astrojs/rss";
 import { sortEntriesByPubDate, slugify } from "../../utils";
 import { getCollection, type CollectionEntry } from "astro:content";
+// Using micromark since its part of the unifiedjs ecosystem Astro already uses and thus will be installed regardless.
 import { micromark } from "micromark";
 
-export const formatLinkLikeFeedItem = ({
+export function formatLinkLikeFeedItem({
 	data,
 	id,
 	body,
-}: CollectionEntry<"links-and-likes">) => ({
-	description: micromark(body),
-	link: `/links-and-likes/${slugify(id)}/`,
-	pubDate: new Date(data.pubDate),
-	title: id,
-});
+}: CollectionEntry<"links-and-likes">) {
+	if (!body) {
+		throw Error(
+			`Tried to format link like feed item "${id}", but object has no body`,
+		);
+	}
+
+	return {
+		content: micromark(body),
+		link: `/links-and-likes/${slugify(id)}/`,
+		pubDate: new Date(data.pubDate),
+		title: id,
+	};
+}
 
 const linkLikes = (await getCollection("links-and-likes")).sort(
 	sortEntriesByPubDate,
